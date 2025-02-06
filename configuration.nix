3123ee763af00f65990b166461c96517aec97d71
raf <rraf@tuta.io>
2025-02-06 23:38:49 +0000

    update: various optional config changes

--- configuration.nix
+++ configuration.nix
@@ -2,7 +2,7 @@
 
 let
   STATE_VERSION = "24.11";
-  DARWIN_STATE_VERSION = 6;
+  DARWIN_STATE_VERSION = 5;
   home-manager = builtins.fetchTarball "https://github.com/nix-community/home-manager/archive/release-${STATE_VERSION}.tar.gz";
 in
 {
@@ -18,11 +18,14 @@ in
     };
   };
   environment.systemPackages = [ pkgs.vim ];
+
+  environment.darwinConfig = "/etc/nix-darwin/configuration.nix";
   system.stateVersion = DARWIN_STATE_VERSION;
   imports = [
     "${home-manager}/nix-darwin"
     ./modules/environment
     ./modules/home
+    ./modules/fonts
   ];
 
   users.users.raf = {
